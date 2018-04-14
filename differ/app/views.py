4cfa23ea08e8306ec910344fea48ad9a0168c0cb
Anledry Cobos <anledry@gmail.com>
2018-04-14 15:13:10 -0500

    Added the code documentation

--- differ/app/views.py
+++ differ/app/views.py
@@ -23,24 +23,47 @@ def create_document(request, pk, endpoint, DocumentModel):
 
 @api_view(['POST'])
 def right(request, pk):
+    """
+    Create the documment for the right side of a diff request.
+    Args:
+        request: The web request.
+        pk: The diff request ID
+    Returns:
+        JSON response for the request.
+    """
     if request.method == 'POST':
         return create_document(request, pk, "right", DocumentRight)
 
 
 @api_view(['POST'])
 def left(request, pk):
+    """
+    Create the documment for the left side of a diff request.
+    Args:
+        request: The web request.
+        pk: The diff request ID
+    Returns:
+        JSON response for the request.
+    """
     if request.method == 'POST':
         return create_document(request, pk, "left", DocumentLeft)
 
 
 @api_view(['GET'])
 def differ(request, pk):
+    """
+    Get the diff result documment between the right and left side diff request.
+    Args:
+        request: The web request.
+        pk: The diff request ID
+    Returns:
+        JSON response for the request.
+    """
     response = ""
     if request.method == 'GET':
         try:
             document_right = DocumentRight.objects.get(pk=pk)
             document_left = DocumentLeft.objects.get(pk=pk)
-            # result = compare(document_right.data, document_left.data)
 
             diff_blocks = None
             equal_size = False
@@ -54,8 +77,6 @@ def differ(request, pk):
                 diff_blocks = get_diff_blocks(document_right.data, document_left.data)
 
             response = JsonResponse({"equal_size": equal_size, "equal_content": equal_content, "diff_blocks": diff_blocks})
-
-
         except DocumentRight.DoesNotExist as e:
             response = JsonResponse({"error": "Could not compare, missing right side data  "})
         except DocumentLeft.DoesNotExist as e:
